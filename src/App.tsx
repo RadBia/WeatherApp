f7bad56914a5e010f47e14e5cba0567251229d3f
Radek <rad.bialkowski@gmail.com>
2023-07-17 10:38:55 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Search } from './components/Search';
+import { ChakraProvider } from '@chakra-ui/react'
+import useAxios from './hooks/useAxios';
+import { FutureWeather } from './components/FutureWeather';
+import { City } from './models/City';
+
+function App() {
+  const [selectedCity, setSelectedCity] = useState<City | null>(null)
+
+  const onSelectedCity = (selectedCity: City) => {
+    setSelectedCity(selectedCity)
+  }
+
+  useAxios()
+
+  return (
+    <ChakraProvider>
+      <Search onSelectedCity={onSelectedCity} />
+      <FutureWeather selectedCity={selectedCity}/>
+    </ChakraProvider>
+  );
+}
+
+export default App;
